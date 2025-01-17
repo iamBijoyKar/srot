7ac047003ba7636d3d6d4fd7f362e697d6e14413
Bijoy Kar <bijoykar54321@gmail.com>
2025-01-17 22:55:13 +0530

    style: Refactor Tailwind CSS configuration to consolidate color definitions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,18 +12,18 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        'brand-purple': 'rgb(var(--brand-purple))',
+        'brand-pink': 'rgb(var(--brand-pink))',
+        'brand-light-pink': 'rgb(var(--brand-light-pink))',
+        'primary-bg': 'rgb(var(--primary-bg))',
+        'secondary-bg': 'rgb(var(--secondary-bg))',
+        'ternary-bg': 'rgb(var(--ternary-bg))',
+        'primary-text': 'rgb(var(--primary-text))',
+        'secondary-text': 'rgb(var(--secondary-text))',
+        'ternary-text': 'rgb(var(--ternary-text))'
       }
-    },
-    colors: {
-      'brand-purple': 'rgb(var(--brand-purple))',
-      'brand-pink': 'rgb(var(--brand-pink))',
-      'brand-light-pink': 'rgb(var(--brand-light-pink))',
-      'primary-bg': 'rgb(var(--primary-bg))',
-      'secondary-bg': 'rgb(var(--secondary-bg))',
-      'ternary-bg': 'rgb(var(--ternary-bg))',
-      'primary-text': 'rgb(var(--primary-text))',
-      'secondary-text': 'rgb(var(--secondary-text))',
-      'ternary-text': 'rgb(var(--ternary-text))'
     }
   },
   plugins: []
