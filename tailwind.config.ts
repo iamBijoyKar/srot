32138398bec67a6597da71ac50f0168b5f74fc97
Bijoy Kar <bijoykar54321@gmail.com>
2025-01-17 20:50:21 +0530

    config: Add custom color variables and dark mode support in Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,25 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
     },
+    colors: {
+      'brand-purple': 'rgb(var(--brand-purple))',
+      'brand-pink': 'rgb(var(--brand-pink))',
+      'brand-light-pink': 'rgb(var(--brand-light-pink))',
+      'primary-bg': 'rgb(var(--primary-bg))',
+      'secondary-bg': 'rgb(var(--secondary-bg))',
+      'ternary-bg': 'rgb(var(--ternary-bg))'
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
