73d4a311d8dc2a091d0189c7123be2502c667d93
Bijoy Kar <bijoykar54321@gmail.com>
2025-01-17 17:38:08 +0530

    feat: Add Editor component with customizable text nodes and flow functionality

---
+++ src/components/Editor/Nodes/TextNode.tsx
@@ -0,0 +1,84 @@
+'use client'
+import { useCallback, useState, useRef, useEffect } from 'react'
+import {
+  Handle,
+  Position,
+  Node,
+  NodeProps,
+  NodeResizeControl
+} from '@xyflow/react'
+
+type TextNodeProps = Node<{
+  text: string
+}>
+
+export default function TextUpdaterNode(props: NodeProps<TextNodeProps>) {
+  const nodeRef = useRef<HTMLDivElement>(null)
+  const inputRef = useRef<HTMLTextAreaElement>(null)
+  const [text, setText] = useState(props.data.text)
+  const [showInput, setShowInput] = useState(false)
+  const width = 200
+  const height = 100
+
+  const onChange = useCallback((event: any) => {
+    setText(event.target.value)
+  }, [])
+
+  const handleNodeClick = useCallback(() => {
+    if (props.selected) {
+      setShowInput(true)
+      inputRef.current?.focus()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (props.selected === false) {
+      setShowInput(false)
+    }
+  }, [props.selected])
+
+  return (
+    <>
+      <NodeResizeControl
+        color="transparent"
+        minWidth={width}
+        minHeight={height}
+        className="relative"
+      >
+        {props.selected ? (
+          <div className="absolute -top-0 -left-0 w-2 h-2 rounded-full bg-gradient-to-r from-purple-600 to-blue-600"></div>
+        ) : null}
+      </NodeResizeControl>
+
+      <Handle type="target" position={Position.Top} />
+      <div
+        ref={nodeRef}
+        onClick={handleNodeClick}
+        className={`border p-2 bg-white rounded w-full h-full relative ${
+          props.selected ? 'border-blue-500' : 'border-gray-300'
+        }`}
+      >
+        {showInput ? (
+          <textarea
+            ref={inputRef}
+            id="text"
+            name="text"
+            onChange={onChange}
+            value={text}
+            className="nodrag outline-none w-full h-full resize-none"
+            spellCheck="false"
+            data-ms-editor="true"
+          ></textarea>
+        ) : (
+          <p
+            className=" whitespace-normal break-words"
+            style={{ width: width - 16, height: height - 16 }}
+          >
+            {text}
+          </p>
+        )}
+      </div>
+      <Handle type="source" position={Position.Bottom} id="a" />
+    </>
+  )
+}
