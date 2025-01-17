091e438cfc9a9ef0ee568ce2cd1395dee3732901
Bijoy Kar <bijoykar54321@gmail.com>
2025-01-18 02:38:36 +0530

    style: Change text color to white in Header component for better visibility

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             href="/sign-up"
             className="active:translate-y-1 transition-all duration-100"
           >
-            <span className="sm:w-[200px] flex items-center gap-2 overflow-hidden text-primary-text px-4 py-2 bg-gradient-to-r from-brand-purple to-brand-pink rounded-full hover:drop-shadow-[0_35px_35px_rgb(var(--brand-purple))] transition-all duration-100">
+            <span className="sm:w-[200px] flex items-center gap-2 overflow-hidden text-white px-4 py-2 bg-gradient-to-r from-brand-purple to-brand-pink rounded-full hover:drop-shadow-[0_35px_35px_rgb(var(--brand-purple))] transition-all duration-100">
               <MdOutlineCelebration className="w-6 h-6" />
               <span className="hidden sm:block ">SignUp for Free</span>
             </span>
