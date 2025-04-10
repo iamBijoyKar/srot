7e60350a44da0df88a046c7d1639b1ccdba62543
Bijoy Kar <bijoykar54321@gmail.com>
2025-04-10 13:12:35 +0530

    update: Layout Metadata (title) updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Srot',
+  title: 'Srot | Design Workflows Effortlessly',
   description: 'Srot is a web application for managing your tasks.'
 }
 
