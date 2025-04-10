e8fdce02ff7799538344b031a01139e3d82c9f6a
Bijoy Kar <bijoykar54321@gmail.com>
2025-04-10 21:40:22 +0530

    update: Footer added in the auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import './../globals.css'
 import Header from '@/components/Auth/Header'
+import Footer from '@/components/Home/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({
         <body className={`${inter.className} bg-primary-bg `}>
           <Header />
           <main className="w-full h-full">{children}</main>
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
